41f19103eaea6ada7070e0923e33b5097c1e1633
amita <amitasher4@gmail.com>
2022-01-04 01:22:05 +0200

    fix responsive project

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,22 +3,22 @@ import '../css/aboutMe.css'
 
 const AboutMe = () => {
   return (
-     <div className='backgroundAboutMe' id='aboutMe'>
-       <h1 className='titleSectionTwo'>About me</h1>
-       <div className="divSection2">
-            <div className="lineCenter" >
-                <hr/>
-            </div>
-         
-            <p className="textSection2">Hi I'm Amit Asher 27 years old ,I'm currently living in Herzeliya ,I have over two years of experience in the field of development.
-             Full Stack Developer experienced in JavaScript based programming with a background in frontend
-and backend development. Independent learner, and team worker. Determined hard worker and
-solution-oriented. Experience with ensuring applications security and ability to interact with API and
-DataBases.</p>
+    <div className='backgroundAboutMe' id='aboutMe'>
+      <h1 className='titleSectionTwo'>About me</h1>
+      <div className="divSection2">
+        <div className="lineCenter" >
+          <hr />
         </div>
- 
+
+        <p className="textSection2">Hi I'm Amit Asher 27 years old ,I'm currently living in Herzeliya ,I have over two years of experience in the field of development.
+          Full Stack Developer experienced in JavaScript based programming with a background in frontend
+          and backend development. Independent learner, and team worker. Determined hard worker and
+          solution-oriented. Experience with ensuring applications security and ability to interact with API and
+          DataBases.</p>
+      </div>
+
     </div>
-   );
+  );
 }
- 
+
 export default AboutMe;
